14305374eeb931736a392a1010edc0fcfaf7c619
Tamal Anwar <tamalanwaronline@gmail.com>
2020-02-24 18:39:54 +0600

    Comments and body parser

--- server.js
+++ server.js
@@ -4,13 +4,21 @@
 
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+// Body parser for parsing JSON payloads and query strings
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
   res.end('Hello Again Node JS!');
 });
 
-const PORT = 5000;
+// The port and the listen function
+// Get the PORT from the server like Heroku or default to 5000 development port.
+const PORT = process.env.PORT || 5000;
 
+// Start the engine
 app.listen(PORT, function() {
   console.log('Express server running on PORT', PORT);
 });
